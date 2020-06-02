c2b541102a1c86612252517b34ca718f49b0f5dc
Darsh <darshkpatel@gmail.com>
2020-06-02 23:16:13 +0530

    Use Grid , Added Text

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,8 +1,10 @@
 import Head from 'next/head';
-import { Box, Button } from 'grommet';
+import {
+  Box, Button, Grid, Heading, Text,
+} from 'grommet';
 import { Github } from 'grommet-icons';
 
-const LoginPage = (props) => (
+const LoginPage = () => (
   <>
     <Head>
       <title>Cross Pods Login</title>
@@ -16,31 +18,42 @@ const LoginPage = (props) => (
     >
 
 
-      <Box
-        pad="medium"
-        alignContent="between"
-        direction="row"
-        basis="1/3"
+      <Grid
+        fill
+        columns={['1/4', '3/4']}
+        rows={['1/3', '1/3', '1/3']}
+        gap="small"
+        areas={[
+          ['left-top', 'right'],
+          ['left-mid', 'right'],
+          ['left-bottom', 'right'],
+        ]}
       >
+        <Heading gridArea="left-top" alignSelf="end" margin="medium">Cross Pods</Heading>
+        <Text gridArea="left-top" alignSelf="end" margin={{ top: 'large', left: 'medium' }}>Final Piece To The MLH Fellowship Experience </Text>
 
-        <Box
-          justify="center"
-          height="100px"
-          width="300px"
-          alignSelf="center"
-          align="center"
-        >
-          <Button
-            size="small"
+        {/* left side , middle box in grid */}
+        <Box gridArea="left-mid" direction="row">
+          {/* Github Login Button */}
+
+          <Box
+            justify="center"
+            height="100px"
+            width="300px"
+            alignSelf="start"
+            align="center"
           >
-            <Box pad="small" direction="row" align="center" gap="small" background="black">
-              <Github />
-              {' '}
-              Login With Github
-            </Box>
-          </Button>
+            <Button size="small">
+              <Box pad="small" direction="row" align="center" gap="small" background="black">
+                <Github />
+                {' '}
+                Login With Github
+              </Box>
+            </Button>
+          </Box>
         </Box>
-      </Box>
+      </Grid>
+
     </Box>
 
   </>
