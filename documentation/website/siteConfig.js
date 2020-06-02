0ac040888cc6b7960841148eed4cbc97f3268329
Darsh <darshkpatel@gmail.com>
2020-06-02 13:11:52 +0530

    Basic Config

--- documentation/website/siteConfig.js
+++ documentation/website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'Cross Pods', // Title for your website.
+  tagline: 'The final piece to MLH Fellowship Experience',
+  url: 'https://cross-pods.now.sh', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'cross-pods',
+  organizationName: 'cross-pods',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -71,7 +71,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Cross Pods`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
