b1da03bb956d0f021fd501dcac7025955684df2c
Chua Bing Quan <chipneverdies@gmail.com>
2020-06-03 22:18:53 +0800

    Complete bubble interface with mock data

--- pages/discover.jsx
+++ pages/discover.jsx
@@ -10,11 +10,41 @@ import Graph from 'react-graph-vis';
 export default function Discover() {
   const graph = {
     nodes: [
-      { id: 1, label: 'Node 1', title: 'node 1 tootip text' },
-      { id: 2, label: 'Node 2', title: 'node 2 tootip text' },
-      { id: 3, label: 'Node 3', title: 'node 3 tootip text' },
-      { id: 4, label: 'Node 4', title: 'node 4 tootip text' },
-      { id: 5, label: 'Node 5', title: 'node 5 tootip text' },
+      {
+        id: 1,
+        // label: 'Node 1',
+        title: 'node 1 tootip text',
+        shape: 'image',
+        image: 'https://cdn4.iconfinder.com/data/icons/linecon/512/photo-512.png',
+      },
+      {
+        id: 2,
+        // label: 'Node 2',
+        title: 'node 2 tootip text',
+        shape: 'image',
+        image: 'https://cdn4.iconfinder.com/data/icons/linecon/512/photo-512.png',
+      },
+      {
+        id: 3,
+        // label: 'Node 3',
+        title: 'node 3 tootip text',
+        shape: 'image',
+        image: 'https://cdn4.iconfinder.com/data/icons/linecon/512/photo-512.png',
+      },
+      {
+        id: 4,
+        // label: 'Node 4',
+        title: 'node 4 tootip text',
+        shape: 'image',
+        image: 'https://cdn4.iconfinder.com/data/icons/linecon/512/photo-512.png',
+      },
+      {
+        id: 5,
+        // label: 'Node 5',
+        title: 'node 5 tootip text',
+        shape: 'image',
+        image: 'https://cdn4.iconfinder.com/data/icons/linecon/512/photo-512.png',
+      },
     ],
     edges: [],
   };
