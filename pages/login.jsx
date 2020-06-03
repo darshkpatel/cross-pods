3dc0726ec5bd532bc7a5d443660ce533481f7713
Anshul Goyal <32068075+anshulrgoyal@users.noreply.github.com>
2020-06-03 12:22:52 +0530

    [API] Adding authentication using github oAuth (#9)
    * Added basic boilerplate for github OAuth
    
    * Redirection Logic
    
    * Updated Background Image
    
    * Added gormmet icons, sass
    
    * update eslint ignores
    
    * basic login button ui
    
    * Removed unused styling
    
    * Use Grid , Added Text
    
    * added vercel gitignore
    
    (cherry picked from commit 8a917c189512125a6369933430c2bd47f977245a)
    
    * Remove vercel variables
    
    * BugFix: Text overlapping in some viewports
    
    * Removed redirect
    
    * Fixed yarn.lock
    
    Co-authored-by: Darsh <darshkpatel@gmail.com>

---
+++ pages/login.jsx
@@ -0,0 +1,67 @@
+import Head from 'next/head';
+import {
+  Box, Button, Grid, Heading, Text,
+} from 'grommet';
+import { Github } from 'grommet-icons';
+
+const LoginPage = () => (
+  <>
+    <Head>
+      <title>Cross Pods Login</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Box
+      direction="row"
+      background={{ image: 'url(/cross-pod-bg.png)' }}
+      fill
+    >
+
+
+      <Grid
+        fill
+        columns={['1/4', '3/4']}
+        rows={['1/3', '1/3', '1/3']}
+        gap="small"
+        areas={[
+          ['left-top', 'right'],
+          ['left-mid', 'right'],
+          ['left-bottom', 'right'],
+        ]}
+      >
+
+        <Box gridArea="left-top" direction="row">
+          <Box alignSelf="end" alignContent="center">
+            <Heading textAlign="center" margin={{ bottom: 'xsmall' }}>Cross Pods</Heading>
+            <Text textAlign="center">Final Piece To The MLH Fellowship Experience </Text>
+          </Box>
+        </Box>
+
+        {/* left side , middle box in grid */}
+        <Box gridArea="left-mid" direction="row">
+          {/* Github Login Button */}
+
+          <Box
+            justify="center"
+            height="100px"
+            width="400px"
+            alignSelf="start"
+            align="center"
+          >
+            <Button size="small">
+              <Box pad="small" direction="row" align="center" gap="small" background="black">
+                <Github />
+                {' '}
+                Login With Github
+              </Box>
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+
+    </Box>
+
+  </>
+);
+
+export default LoginPage;
