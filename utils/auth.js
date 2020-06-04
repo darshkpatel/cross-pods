2cc8d7d1516455be0c6caa4908b50508b20fca0a
Chua Bing Quan <chipneverdies@gmail.com>
2020-06-04 15:21:21 +0800

    Refactor auth.js to comply with linter

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,13 @@
 import { initAuth0 } from '@auth0/nextjs-auth0';
 
 export default initAuth0({
-    domain: process.env.AUTH0_DOMAIN,
-    clientId: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    scope: 'openid profile',
-    redirectUri: `${process.env.NEXT_PUBLIC_HOSTNAME}/api/callback`,
-    postLogoutRedirectUri: process.env.NEXT_PUBLIC_HOSTNAME,
-    session: {
-        cookieSecret: process.env.COOKIE_SECRET,
-    },
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  scope: 'openid profile',
+  redirectUri: `${process.env.NEXT_PUBLIC_HOSTNAME}/api/callback`,
+  postLogoutRedirectUri: process.env.NEXT_PUBLIC_HOSTNAME,
+  session: {
+    cookieSecret: process.env.COOKIE_SECRET,
+  },
 });
